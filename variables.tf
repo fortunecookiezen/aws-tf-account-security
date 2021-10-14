dfd53076f7f2599c8a5e1111348284c6c2d78490
James Phillips <jphillips@fnni.com>
2021-10-13 21:15:30 -0700

    assigning default values to booleans

--- variables.tf
+++ variables.tf
@@ -2,8 +2,10 @@ variable "account_alias" {
   type = string
 }
 variable "create_access_analyzer" {
-  type = bool
+  type    = bool
+  default = true
 }
 variable "create_s3_public_access_block" {
-  type = bool
+  type    = bool
+  default = true
 }
